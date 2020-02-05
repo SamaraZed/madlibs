29e9df956bac901159119f627e81010a74e02052
Ayham Samara <Ayham.Samara@live.com>
2020-02-05 14:47:34 +0200

    fixed indentation in main function

--- madlibs.py
+++ madlibs.py
@@ -1,18 +1,18 @@
 def main():
 	# write your code here
 
- time = input ("Enter a number from 1 to 12: ")
- items = input ("Enter a noun (plural): ")
- name =  input ("Enter a name: ").title()
- scream = input ("Enter any sentence: ").upper()
- action = input ("Enter a verb:")
+    time = input ("Enter a number from 1 to 12: ")
+    items = input ("Enter a noun (plural): ")
+    name =  input ("Enter a name: ").title()
+    scream = input ("Enter any sentence: ").upper()
+    action = input ("Enter a verb:")
 
- print ("The story goes...")
+    print ("The story goes...")
 
- print ("It was " ,time, "o'clock when I heard a knock at the door."\
- "I opened the door and there was a box full of " ,items," with a note saying From " ,name,"."\
- "Just as I closed the door I heard a scream ",scream,\
- "I froze in place and all I could do was " ,action,".")
+    print ("It was " ,time, "o'clock when I heard a knock at the door."\
+    "I opened the door and there was a box full of " ,items," with a note saying From " ,name,"."\
+    "Just as I closed the door I heard a scream ",scream,\
+    "I froze in place and all I could do was " ,action,".")
 
 
 if __name__ == '__main__':
